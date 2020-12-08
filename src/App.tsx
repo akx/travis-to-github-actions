50a056537515bde71857f37abc467bb332b76971
Aarni Koskela <akx@iki.fi>
2020-12-08 14:25:32 +0200

    Fix Eslint complaints

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Textarea } from "@chakra-ui/react";
+import { Flex, Heading, Textarea } from "@chakra-ui/react";
 import React from "react";
 import "./App.css";
 import { convertToGHA } from "./converter/converter";
