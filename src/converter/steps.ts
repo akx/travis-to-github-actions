e6e3dea88e10e69a42f4c0b646e7cf9accb8c05e
Aarni Koskela <akx@iki.fi>
2020-12-09 15:30:39 +0200

    Support before_*/after_* a little better

--- src/converter/steps.ts
+++ src/converter/steps.ts
@@ -1,6 +1,7 @@
 import { JobContext } from "./types";
 import { Step } from "./types/github-workflow";
 import { arrayfy } from "./utils";
+import { Travis } from "./types/travis";
 
 function getStepEnvFragment(ctx: JobContext) {
   if (!Object.keys(ctx.environmentVariables).length) {
@@ -11,51 +12,71 @@ function getStepEnvFragment(ctx: JobContext) {
   );
 }
 
+const stepSources: (keyof Travis)[] = [
+  "before_install",
+  "install",
+  "before_script",
+  "script",
+  "after_script",
+  "after_success",
+];
+
+const sourceWarnings: Record<string, string> = {
+  after_success: "after_success actions were concatenated",
+  before_install: "before_install actions were prepended",
+};
+
+function convertJobStepArray(
+  ctx: JobContext,
+  sourceName: keyof Travis,
+  stepModifier: (step: Step) => Step
+) {
+  // TODO: would be nice to add comments/names for non-script steps
+  let stepsGenerated = 0;
+  arrayfy(ctx.travis[sourceName]).forEach((command) => {
+    if (typeof command !== "string") {
+      ctx.messages.push({
+        type: "warning",
+        text: `Entry not a string in ${sourceName}: ${command}`,
+      });
+      return;
+    }
+    ctx.steps.push(
+      stepModifier({
+        run: command,
+      })
+    );
+    stepsGenerated++;
+  });
+  if (stepsGenerated > 0) {
+    delete ctx.travis[sourceName];
+    const warning = sourceWarnings[sourceName];
+    if (warning) {
+      ctx.messages.push({
+        type: "warning",
+        text: warning,
+      });
+    }
+  }
+  return stepsGenerated;
+}
+
 export function convertJobSteps(ctx: JobContext) {
-  const { travis, messages, steps } = ctx;
   const env = getStepEnvFragment(ctx);
 
-  steps.push({
-    uses: "actions/checkout@v2",
-  });
-
-  function maybeInjectEnv(step: Step): Step {
+  const maybeInjectEnv = (step: Step): Step => {
     // If the step looks like it uses an environment variable, inject all of them into the step
     if (env && step.run && step.run.includes("$")) {
       step.env = env;
     }
     return step;
-  }
+  };
 
-  arrayfy(travis.install).forEach((command) => {
-    steps.push(
-      maybeInjectEnv({
-        run: command,
-      })
-    );
+  ctx.steps.push({
+    uses: "actions/checkout@v2",
   });
-  delete travis.install;
 
-  arrayfy(travis.script).forEach((command) => {
-    steps.push(
-      maybeInjectEnv({
-        run: command,
-      })
-    );
-  });
-  delete travis.script;
-  if (travis.after_success) {
-    arrayfy(travis.after_success).forEach((command) => {
-      steps.push(
-        maybeInjectEnv({
-          run: command,
-        })
-      );
-    });
-    messages.push({
-      type: "warning",
-      text: "After-success are concatenated onto regular steps",
-    });
-    delete travis.after_success;
+  for (let source of stepSources) {
+    convertJobStepArray(ctx, source, maybeInjectEnv);
   }
 }
